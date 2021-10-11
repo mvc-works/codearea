ed6b5d3ed52871cdcc5aaf87dd11f82c9721829a
jiyinyiyong <jiyinyiyong@gmail.com>
2021-10-12 00:26:46 +0800

    switching to typescript; tag 0.0.3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-
-// import coffeescript from 'rollup-plugin-coffee-script';
-import coffee from "vite-plugin-coffee";
-
 export default {
-  plugins: [
-    coffee({
-      // Set to true if you use react
-      jsx: false,
-    }),
-  ]
-}
+  plugins: [],
+};
